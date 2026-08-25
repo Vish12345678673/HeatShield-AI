a8e2bba2a368828357709fcb8aaf5fc18e59f7b1
Vish12345678673 <vishwajadhav59@gmail.com>
2026-08-25 23:59:40 +0530

    Fix TanStack Start Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: { port: 5173, host: "0.0.0.0" },
-  resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
+
+  resolve: {
+    tsconfigPaths: true,
+  },
+
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
+    viteReact(),
+  ],
 });
